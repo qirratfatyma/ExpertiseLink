b59f2ebc74386223a7bd732d54f60d24e3da61ef
Syeda Qirrat Fatima <syedaqirratfatima@gmail.com>
2023-12-04 22:06:30 +0500

    Sign Up Updated

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -4,23 +4,107 @@ function Signup() {
   return (
     <div className="Signup-container">
       <form className="form-container">
-        <h2>Sign up Form</h2>
-        <label for="name">
-          <div className="form-section">
+        <h2 style={{ textAlign: "left", fontSize: "24px" }}>Sign up Form</h2>
+
+        <div className="form-section">
+          <label htmlFor="firstName">
             <h5>
               First Name <span style={{ color: "red" }}>*</span>
             </h5>
-          </div>
-        </label>
-        <br />
-        <input
-          type="text"
-          placeholder="first name"
-          id="username"
-          name="username"
-          required
-        />
-        <br />
+          </label>
+          <br />
+          <input
+            type="text"
+            placeholder="First Name"
+            id="firstName"
+            name="firstName"
+            required
+          />
+        </div>
+
+        <div className="form-section">
+          <label htmlFor="lastName">
+            <h5>
+              Last Name <span style={{ color: "red" }}>*</span>
+            </h5>
+          </label>
+          <br />
+          <input
+            type="text"
+            placeholder="Last Name"
+            id="lastName"
+            name="lastName"
+            required
+          />
+        </div>
+
+        <div className="form-section">
+          <label htmlFor="email">
+            <h5>
+              Email <span style={{ color: "red" }}>*</span>
+            </h5>
+          </label>
+          <br />
+          <input
+            type="email"
+            placeholder="Email"
+            id="email"
+            name="email"
+            required
+          />
+        </div>
+
+        <div className="form-section">
+          <label htmlFor="companyName">
+            <h5>
+              Company Name <span style={{ color: "red" }}>*</span>
+            </h5>
+          </label>
+          <br />
+          <input
+            type="text"
+            placeholder="Company Name"
+            id="companyName"
+            name="companyName"
+            required
+          />
+        </div>
+
+        <div className="form-section">
+          <label htmlFor="role">
+            <h5>
+              Role <span style={{ color: "red" }}>*</span>
+            </h5>
+          </label>
+          <br />
+          <input
+            type="text"
+            placeholder="Role"
+            id="role"
+            name="role"
+            required
+          />
+        </div>
+
+        <div className="form-section">
+          <label htmlFor="countryRegion">
+            <h5>
+              Country/Region <span style={{ color: "red" }}>*</span>
+            </h5>
+          </label>
+          <br />
+          <input
+            type="text"
+            placeholder="Country/Region"
+            id="countryRegion"
+            name="countryRegion"
+            required
+          />
+        </div>
+
+        <button type="submit" style={{ fontSize: "18px", }}>
+          Sign Up
+        </button>
       </form>
     </div>
   );
