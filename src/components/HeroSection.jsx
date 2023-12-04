37f501c8b835c9fe10d31cd4704e80a5445ed05c
Syeda Qirrat Fatima <syedaqirratfatima@gmail.com>
2023-12-05 00:11:52 +0500

    added languages

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import logo from "../assets/img/logo.png";
 import heroImage from "../assets/img/image-1.png";
+import Node from "../assets/img/node.png";
+import php from "../assets/img/php.png";
+import react from "../assets/img/react.png";
+import vuejs from "../assets/img/vuejs.png";
+import javaimg from "../assets/img/javaimg.png"
+import angukar from "../assets/img/angukar.png";
+import { parsePath } from 'react-router-dom';
 
 const Herosection = () => {
   return (
@@ -19,6 +26,35 @@ const Herosection = () => {
               <p>
                 The ultimate hub for knowledge exchange! Unleash your expertise and join a vibrant community where learning knows no bounds. Swap skills, forge connections, and elevate your mastery in a collaborative ecosystem like never before.
               </p>
+            </div>
+          </div>
+        </div>
+      </section>
+        <br></br>
+      {/* Officially Supported Languages Section */}
+      <section className="supported-languages-section">
+        <div className="container">
+          <h2 style={{ color: "#6a3f3e" }}>Officially Supported Languages</h2>
+          <br></br>
+          <br></br>
+          <div className="row">
+            <div className="col-md-2">
+              <img src={Node} alt="Language 1" style={{ width: "100%", height: "auto" }} />
+            </div>
+            <div className="col-md-2">
+              <img src={php} alt="Language 2" style={{ width: "100%", height: "auto" }} />
+            </div>
+            <div className="col-md-2">
+              <img src={react} alt="Language 3" style={{ width: "100%", height: "auto" }} />
+            </div>
+            <div className="col-md-2">
+              <img src={vuejs} alt="Language 4" style={{ width: "100%", height: "auto" }} />
+            </div>
+            <div className="col-md-2">
+              <img src={javaimg} alt="Language 5" style={{ width: "100%", height: "auto" }} />
+            </div>
+            <div className="col-md-2">
+              <img src={angukar} alt="Language 6" style={{ width: "100%", height: "auto" }} />
             </div>
           </div>
         </div>
