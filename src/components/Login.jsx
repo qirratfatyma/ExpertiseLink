a589f7091733550032c49740b296aed25ea27c4a
Syeda Qirrat Fatima <syedaqirratfatima@gmail.com>
2023-12-04 22:26:50 +0500

    Log In Final

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 function Login() {
   return (
-    <div className="Login-container">
+    <div className="Login-container" style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh" }}>
       <form className="form-container">
-        <h2 style={{ textAlign: "left", fontSize: "24px" }}>Log In</h2>
+        <h2 style={{ textAlign: "center", fontSize: "24px" }}>Log In</h2>
 
         <div className="form-section">
           <label htmlFor="email">
@@ -37,12 +37,12 @@ function Login() {
             required
           />
         </div>
-
+        <br></br>
         <button type="submit" style={{ fontSize: "18px", backgroundColor: "#6a3f3e", color: "white" }}>
           Log In
         </button>
 
-        <p style={{ textAlign: "left", marginTop: "10px" }}>
+        <p style={{ textAlign: "center", marginTop: "10px" }}>
           New to ExpertiseLink?{" "}
           <a href="/signup" style={{ color: "#6a3f3e", textDecoration: "underline" }}>
             Sign up
